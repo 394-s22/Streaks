34f41b642a1855a81d89c7e7b89977f59cf5d0d5
Nikhil Aggarwal <nikhil0929@gmail.com>
2022-04-01 12:12:34 -0500

    Created first page

---
+++ src/pages/GroupPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Typography from '@mui/material/Typography';
+
+function GroupPage(props) {
+    return (
+        <div>
+            <Typography variant="h2" gutterBottom component="div">
+                Streaks
+            </Typography>
+        </div>
+    )
+}
+
+export default GroupPage
