b832af4289f43ac5083cacc15eb600e48151ef7f
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2022-04-08 17:33:33 -0500

    line comment about date

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,14 @@ import * as data from "./data";
 
 const App: React.FunctionComponent = () => {
   const currGroup = "0";
-  const [users, setUsers] = useState(data.users);
   const currUser = "2";
 
+  // Current date in the form YYYY-MM-DD
   const date = new Date().toISOString().substring(0, 10);
 
   return (
     <div className="App">
-      <GroupPage
-        currentGroup={currGroup}
-
-        currentUser={currUser}
-        date={date}
-      />
+      <GroupPage currentGroup={currGroup} currentUser={currUser} date={date} />
     </div>
   );
 };
