be055cad908ca4682d0e664f1965e25e60cdf192
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2022-04-13 09:29:13 -0500

    update feed card date, buttons, caption

--- src/components/FeedCard.tsx
+++ src/components/FeedCard.tsx
@@ -1,22 +1,22 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { UserInfo } from '../lib/types';
-import {getImageUrl} from '../utilities/firebaseStorage'
-import { useState, useEffect} from 'react';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { UserInfo } from "../lib/types";
+import { getImageUrl } from "../utilities/firebaseStorage";
+import { useState, useEffect } from "react";
 
 interface FeedCardProps {
   userInfo: UserInfo;
@@ -24,13 +24,11 @@ interface FeedCardProps {
   groupId: string;
 }
 
-
 const FeedCard: React.FunctionComponent<FeedCardProps> = ({
-    userInfo,
-    currentDate,
-    groupId,
-  }) => {
-
+  userInfo,
+  currentDate,
+  groupId,
+}) => {
   const [imgUrl, setImgUrl] = useState("");
 
   function getImgUrl() {
@@ -57,7 +55,12 @@ const FeedCard: React.FunctionComponent<FeedCardProps> = ({
           </IconButton>
         }
         title={userInfo.name}
-        subheader="September 14, 2016"
+        subheader={new Date().toLocaleString("en-US", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
       />
       <CardMedia
         component="img"
@@ -67,21 +70,16 @@ const FeedCard: React.FunctionComponent<FeedCardProps> = ({
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+          Proof caption here (optional)
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
         </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
       </CardActions>
     </Card>
   );
-}
+};
 
-export default FeedCard
+export default FeedCard;
