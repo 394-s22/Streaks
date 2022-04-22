0e1d765879eeef8b5327bb58f878dc9d157d1f01
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2022-04-22 18:53:35 -0500

    removed unused things

--- src/components/FeedCard.tsx
+++ src/components/FeedCard.tsx
@@ -9,10 +9,7 @@ import Typography from "@mui/material/Typography";
 import { UserInfo, GroupProgress } from "../lib/types";
 import { getImageUrl, getImageCaption } from "../utilities/firebaseStorage";
 import { useState, useEffect } from "react";
-import { Chip } from "@mui/material";
-import { setData } from "../utilities/firebase";
 import ReactionButton from "./ReactionButton";
-var randomColor = require("randomcolor");
 
 interface FeedCardProps {
   userInfo: UserInfo;
@@ -31,7 +28,7 @@ const FeedCard: React.FunctionComponent<FeedCardProps> = ({
 }) => {
   const [imgUrl, setImgUrl] = useState("");
   const [imgCaption, setImgCaption] = useState("");
-  const [profileColor, setProfileColor] = useState("purple")
+  const [profileColor] = useState("purple")
 
   function getImgUrl() {
     getImageUrl(userInfo.id, currentDate, groupId).then((data) => {
