ec6599048bf22ac34d9401eafa0aa775b2e1b68b
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2022-04-22 18:01:05 -0500

    create group creation ui part 1

--- src/components/AddProofModal.tsx
+++ src/components/AddProofModal.tsx
@@ -3,22 +3,6 @@ import Modal from "@mui/material/Modal";
 import { Typography } from "@mui/material";
 import React, { useState } from "react";
 import { uploadImage } from "../utilities/firebaseStorage";
-import { FireExtinguisher } from "@mui/icons-material";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "70%",
-  maxWidth: 600,
-  bgcolor: "background.paper",
-  borderRadius: "10px",
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
-};
 
 interface InfoModalProps {
   handleClose: () => void;
