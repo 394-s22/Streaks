5211c5f4c82689090ca34ba58251a12fcc667853
Fardeem Munir <munirfardeem@gmail.com>
2022-04-24 23:42:49 -0500

    Fix styling on user card

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,11 +1,8 @@
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
-import { IconButton, Typography, Icon, Button } from "@mui/material";
+import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
-import React, { useState, useEffect } from "react";
-import { Group, GroupMetaData, AllUsers, UserInfo } from "../lib/types";
-import ChatIcon from "@mui/icons-material/Chat";
-import { getImageUrl } from "../utilities/firebaseStorage";
+import React from "react";
+import { GroupMetaData, UserInfo } from "../lib/types";
 
 interface UserCardProps {
   user: UserInfo;
@@ -23,10 +20,6 @@ const UserCard: React.FunctionComponent<UserCardProps> = ({
   position,
 }) => {
   const progress = group.progress;
-  console.log(date);
-  console.log(group.progress[date]);
-
-  const [proofOpen, setProofOpen] = useState(false);
 
   let medals = [
     <Typography role="img" fontSize={30}>
@@ -39,55 +32,43 @@ const UserCard: React.FunctionComponent<UserCardProps> = ({
       🥉
     </Typography>,
   ];
+
   return (
     <Box
       display="flex"
       flexDirection="row"
-      justifyContent="flex-start"
       alignItems="center"
-      maxWidth="600px"
+      maxWidth="250px"
+      width="100%"
     >
-      <Box
-        display="flex"
-        flexDirection="row"
-        alignItems="center"
-        justifyContent="space-between"
-        width="100%"
-        maxWidth="400px"
-      >
-        <Box
+      <Box sx={{ p: 1 }}>
+        {position <= 2 ? medals[position] : <Box width={30}></Box>}
+      </Box>
+
+      <Box textAlign="left" flex="1">
+        <Typography
           display="flex"
-          flexDirection="row"
+          justifyContent="flex-start"
           alignItems="center"
-          justifyContent="space-between"
-          maxWidth="250px"
-          width="100%"
+          sx={{ fontWeight: 600 }}
         >
-          <Box sx={{ p: 1 }}>
-            {position <= 2 ? medals[position] : <Box width={30}></Box>}
-          </Box>
-          <Box textAlign="left">
-            <Typography
-              display="flex"
-              justifyContent="flex-start"
-              alignItems="center"
-              sx={{ fontWeight: 600 }}
-            >
-              {user.name} {user.id === currentUser ? "(me)" : ""}
-              {!progress[date] ? (
-                <></>
-              ) : progress[date].userIdsWhoCheckedIn.includes(user.id) ? (
-                <CheckCircleIcon color="success" sx={{ ml: 1, fontSize: 20 }} />
-              ) : (
-                <></>
-              )}
-            </Typography>
-            <Typography>
-              Streak: {group.streaks[user.id]} day
-              {group.streaks[user.id] === 1 ? "" : "s"}
-            </Typography>
-          </Box>
-        </Box>
+          {user.name} {user.id === currentUser ? "(me)" : ""}
+        </Typography>
+
+        <Typography>
+          Streak: {group.streaks[user.id]} day
+          {group.streaks[user.id] === 1 ? "" : "s"}
+        </Typography>
+      </Box>
+
+      <Box sx={{ p: 1 }}>
+        {!progress[date] ? (
+          <></>
+        ) : progress[date].userIdsWhoCheckedIn.includes(user.id) ? (
+          <CheckCircleIcon color="success" sx={{ ml: 1, fontSize: 20 }} />
+        ) : (
+          <></>
+        )}
       </Box>
     </Box>
   );
