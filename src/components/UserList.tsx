1999075cd86bbfb1f00283b61b6aeebd91977c2d
Fardeem Munir <munirfardeem@gmail.com>
2022-04-24 23:43:38 -0500

    Make users list nicer

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,7 +1,7 @@
-import { Box, IconButton } from "@mui/material";
+import { Box } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import React from "react";
-import { Group, GroupMetaData, AllUsers } from "../lib/types";
+import { AllUsers, GroupMetaData } from "../lib/types";
 import UserCard from "./UserCard";
 
 interface UserListProps {
@@ -23,28 +23,28 @@ const UserList: React.FunctionComponent<UserListProps> = ({
   });
 
   return (
-    <Box display="flex" justifyContent="center" alignItems="center">
-      <Stack
-        direction="column"
+    <div>
+      <Box
+        display="flex"
         justifyContent="center"
-        alignItems="center"
-        spacing={2}
         width="100%"
-        maxWidth="600"
-        sx={{ p: 2 }}
+        maxWidth={250}
+        margin="0 auto"
       >
-        {sorted.map((pair, index) => (
-          <UserCard
-            key={pair[0]}
-            user={users[pair[0]]}
-            currentUser={currentUser}
-            group={group}
-            date={date}
-            position={index}
-          />
-        ))}
-      </Stack>
-    </Box>
+        <Stack spacing={2} margin="0 auto">
+          {sorted.map((pair, index) => (
+            <UserCard
+              key={pair[0]}
+              user={users[pair[0]]}
+              currentUser={currentUser}
+              group={group}
+              date={date}
+              position={index}
+            />
+          ))}
+        </Stack>
+      </Box>
+    </div>
   );
 };
 
