a84d01345159b7d6c72e61675cf9ee770dbc95e1
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2022-05-23 10:30:13 -0500

    initial test

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+import { MemoryRouter } from "react-router-dom";
+
+test("renders learn react link", () => {
+  render(<App />, { wrapper: MemoryRouter });
+  const linkElement = screen.queryByText(/Loading/);
+  expect(linkElement).toBeInTheDocument();
+});
