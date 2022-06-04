d5d947ca91c594a6410eec247fc5b9f91bbfd767
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2022-06-04 23:28:27 +0000

    finish test

--- src/components/ReactionButton.test.js
+++ src/components/ReactionButton.test.js
@@ -1,11 +1,14 @@
-import { render, screen, debug } from "@testing-library/react";
+import { render, screen, debug, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ReactionButton from "./ReactionButton";
 import { MemoryRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
+import { setData, useData } from "../utilities/firebase";
 
+jest.mock("../utilities/firebase");
 test("clicking a reaction that you've liked before ", async () => {
   const blankFn = jest.fn();
+  setData.mockReturnValue(Promise.resolve({ data: {} }));
   render(
     <ReactionButton
       userInfo={{
@@ -28,4 +31,10 @@ test("clicking a reaction that you've liked before ", async () => {
   );
   const button = screen.getByText("x1");
   expect(button).toBeInTheDocument();
+  userEvent.click(button);
+  await waitFor(() => expect(setData).toHaveBeenCalledTimes(1));
+  expect(setData).toHaveBeenCalledWith(
+    "/groups/0/progress/2022-06-04/userReactions/abc/Like",
+    ["abc"]
+  );
 });
